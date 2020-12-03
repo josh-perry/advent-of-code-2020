bb71344298d11b04f444e200ab5145ce7952ec8c
Josh Perry <joshperry@outlook.com>
2020-12-03 18:40:15 +0000

    Day 1 part 1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 use std::fs;
+use regex::Regex;
+
+struct PasswordPolicy {
+    letter: char,
+    max: i32,
+    min: i32,
+}
+
+struct Password {
+    policy: PasswordPolicy,
+    password: String
+}
 
 fn read_numbers(filename : String) -> Vec<i32> {
     let contents = fs::read_to_string(filename).expect("Failed to read file!");
@@ -41,9 +53,60 @@ fn find_triplet(numbers : &[i32]) -> i32 {
     return -1;
 }
 
-fn main() {
+fn read_passwords(filename : String) -> Vec<Password> {
+    let contents = fs::read_to_string(filename).expect("Failed to read file!");
+    let mut passwords: Vec<Password> = vec![];
+    let re = Regex::new(r"(\d+)\-(\d+) ([A-z]): (.+)").unwrap();
+
+    for line in contents.split("\n") {
+        for cap in re.captures_iter(line) {
+            println!("Min: {}, Max: {}, Letter: {}, Password: {}", &cap[1], &cap[2], &cap[3], &cap[4]);
+
+            let policy = PasswordPolicy {
+                min: cap[1].parse::<i32>().unwrap(),
+                max: cap[2].parse::<i32>().unwrap(),
+                letter: cap[3].chars().next().unwrap()
+            };
+
+            let password = Password {
+                policy: policy,
+                password: cap[4].to_string()
+            };
+
+            passwords.push(password);
+        }
+    }
+
+    return passwords;
+}
+
+fn day1() {
     let numbers = read_numbers("day1_input".to_string());
 
     println!("Pair:    {}", find_pair(&numbers));
     println!("Triplet: {}", find_triplet(&numbers));
+}
+
+fn day2() {
+    let passwords = read_passwords("day2_input".to_string());
+    let mut good_passwords = 0;
+    let mut bad_passwords = 0;
+
+    for password in passwords {
+        let count : i32 = password.password.matches(password.policy.letter).count() as i32;
+
+        if count >= password.policy.min && count <= password.policy.max {
+            good_passwords += 1;
+            continue;
+        }
+
+        bad_passwords += 1;
+    }
+    
+    println!("Good passwords: {}, Bad passwords: {}", good_passwords, bad_passwords);
+}
+
+fn main() {
+    //day1();
+    day2();
 }
