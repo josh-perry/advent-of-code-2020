563e3d059fb031bf44603b2aa0a8392e219b8515
Josh Perry <joshperry@outlook.com>
2020-12-05 15:54:47 +0000

    Day 5 part 2

--- src/day_5.rs
+++ src/day_5.rs
@@ -23,6 +23,7 @@ pub fn find_seat(pass: &str, rows: f32, cols: f32) -> i32 {
 
     println!("{}, {}", current_min_row, current_max_row);
     println!("{}, {}", current_min_col, current_max_col);
+
     return (current_min_row * 8.0 + current_min_col) as i32;
 }
 
@@ -43,6 +44,36 @@ pub fn part_1(filename: String) -> i32 {
     return biggest;
 }
 
-//pub fn part_2(filename: String) {
-//
-//}
+pub fn part_2(filename: String) -> i32 {
+    let contents = fs::read_to_string(filename).expect("Failed to read file!");
+    let mut seats = vec![];
+
+    for line in contents.split("\n") {
+        let seat = find_seat(line, 127.0, 7.0);
+
+        if seat == 0 {
+            println!("{}", line);
+        }
+
+        seats.push(seat);
+    }
+
+    seats.sort();
+
+    let min = seats[0];
+    let max = seats[seats.len() - 1];
+
+    // Loop over them all
+    for c in min..max {
+        // If a seat is missing, check 1 to the left and 1 to the right, if they exist then
+        // we've found our seat!
+        if !seats.iter().any(|&i| i == c) {
+            if seats.iter().any(|&i| i == c + 1) && seats.iter().any(|&i| i == c - 1) {
+                println!("Missing seat is {}", c);
+                return c;
+            }
+        }
+    }
+
+    panic!("Failed to find seat!");
+}
